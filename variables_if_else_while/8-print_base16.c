20e2f0af77a85d55ee110371c456c7561920b615
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-06-22 19:44:24 +0200

    Add Task 8: print base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n + '0');
+
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
