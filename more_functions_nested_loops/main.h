ae2ea904d64304617097697dd7b18f69148fd921
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-06-24 11:42:30 +0200

    Task 8: add print_square

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,13 +6,15 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_most_numbers
-(void);
+void print_most_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
 void more_numbers(void);
 
 #endif
+
+
 
 
 
