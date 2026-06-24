9f4e16ff69150298007a411685e15721689215cb
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-06-24 12:21:37 +0200

    Task 10: add print_triangle

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -10,9 +10,11 @@ void print_most_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
 void more_numbers(void);
 
 #endif
+
 
 
 
