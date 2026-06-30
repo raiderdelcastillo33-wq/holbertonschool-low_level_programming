e340916844c6359705dc50283adb76ad0af3fbe0
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-06-30 16:14:30 +0200

    Add Task 0: calculator skeleton

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the simple calculator program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int choice;
+
+	printf("Simple Calculator\n");
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Quit\n");
+	printf("Choice: ");
+	scanf("%d", &choice);
+
+	if (choice == 0)
+	{
+		printf("Bye!\n");
+	}
+
+	return (0);
+}
+
