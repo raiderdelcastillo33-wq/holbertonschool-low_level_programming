62160c01bc06f65a8a5806aed6c9f411fd0659eb
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-06-30 19:13:55 +0200

    Add Task 4: multiplication operation

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
 
 /**
+ * print_menu - Prints the calculator menu.
+ */
+void print_menu(void)
+{
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Quit\n");
+	printf("Choice: ");
+}
+
+/**
+ * handle_operation - Handles the selected calculator operation.
+ * @choice: User menu choice.
+ */
+void handle_operation(int choice)
+{
+	int a, b, result;
+
+	if (choice < 0 || choice > 4)
+		printf("Invalid choice\n");
+	if (choice == 1)
+	{
+		printf("A: ");
+		scanf("%d", &a);
+		printf("B: ");
+		scanf("%d", &b);
+		result = a + b;
+		printf("Result: %d\n", result);
+	}
+	if (choice == 2)
+	{
+		printf("A: ");
+		scanf("%d", &a);
+		printf("B: ");
+		scanf("%d", &b);
+		result = a - b;
+		printf("Result: %d\n", result);
+	}
+	if (choice == 3)
+	{
+		printf("A: ");
+		scanf("%d", &a);
+		printf("B: ");
+		scanf("%d", &b);
+		result = a * b;
+		printf("Result: %d\n", result);
+	}
+}
+
+/**
  * main - Entry point for the simple calculator program.
  *
  * Return: Always 0.
@@ -8,37 +60,12 @@
 int main(void)
 {
 	int choice;
-	int a, b, result;
 
 	printf("Simple Calculator\n");
 	do {
-		printf("1) Add\n");
-		printf("2) Subtract\n");
-		printf("3) Multiply\n");
-		printf("4) Divide\n");
-		printf("0) Quit\n");
-		printf("Choice: ");
+		print_menu();
 		scanf("%d", &choice);
-		if (choice < 0 || choice > 4)
-			printf("Invalid choice\n");
-		if (choice == 1)
-		{
-			printf("A: ");
-			scanf("%d", &a);
-			printf("B: ");
-			scanf("%d", &b);
-			result = a + b;
-			printf("Result: %d\n", result);
-		}
-		if (choice == 2)
-		{
-			printf("A: ");
-			scanf("%d", &a);
-			printf("B: ");
-			scanf("%d", &b);
-			result = a - b;
-			printf("Result: %d\n", result);
-		}
+		handle_operation(choice);
 	} while (choice != 0);
 
 	printf("Bye!\n");
