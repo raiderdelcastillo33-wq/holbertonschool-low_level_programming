df2583c60cc3923cc9d4b0daced2ee7088fc694a
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-06-30 14:06:32 +0200

    Add Task 7: print second half of string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string.
+ * @str: String to print.
+ */
+void puts_half(char *str)
+{
+	int len;
+	int start;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len + 1) / 2;
+
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+
+	_putchar('\n');
+}
