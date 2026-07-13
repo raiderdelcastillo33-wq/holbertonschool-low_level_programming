2a900cef9661b00bd9a6cc8d8983e20684288059
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-07-13 08:25:34 +0200

    Add recursive factorial function

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number
+ * @n: Number to calculate
+ *
+ * Return: Factorial of n, or -1 if n is negative
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
