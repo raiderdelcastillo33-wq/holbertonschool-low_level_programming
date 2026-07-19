78830d873ad1f19a8286c33392a618580fd3ece1
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-07-19 21:03:06 +0200

    Add invalid input recovery

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -14,16 +14,44 @@ void print_menu(void)
 }
 
 /**
+ * clear_input - Discards invalid characters from standard input.
+ */
+void clear_input(void)
+{
+	int c;
+
+	while ((c = getchar()) != '\n' && c != EOF)
+		;
+}
+
+/**
  * read_operands - Reads two integer operands.
  * @a: Pointer to the first operand.
  * @b: Pointer to the second operand.
  */
 void read_operands(int *a, int *b)
 {
-	printf("A: ");
-	scanf("%d", a);
-	printf("B: ");
-	scanf("%d", b);
+	while (1)
+	{
+		printf("A: ");
+
+		if (scanf("%d", a) == 1)
+			break;
+
+		printf("Invalid number\n");
+		clear_input();
+	}
+
+	while (1)
+	{
+		printf("B: ");
+
+		if (scanf("%d", b) == 1)
+			break;
+
+		printf("Invalid number\n");
+		clear_input();
+	}
 }
 
 /**
@@ -36,28 +64,36 @@ void handle_operation(int choice)
 	double div_result;
 
 	if (choice < 0 || choice > 4)
+	{
 		printf("Invalid choice\n");
+		return;
+	}
+
 	if (choice == 1)
 	{
 		read_operands(&a, &b);
 		result = a + b;
 		printf("Result: %d\n", result);
 	}
+
 	if (choice == 2)
 	{
 		read_operands(&a, &b);
 		result = a - b;
 		printf("Result: %d\n", result);
 	}
+
 	if (choice == 3)
 	{
 		read_operands(&a, &b);
 		result = a * b;
 		printf("Result: %d\n", result);
 	}
+
 	if (choice == 4)
 	{
 		read_operands(&a, &b);
+
 		if (b == 0)
 			printf("Error: division by zero\n");
 		else
@@ -78,11 +114,23 @@ int main(void)
 	int choice;
 
 	printf("Simple Calculator\n");
-	do {
+
+	while (1)
+	{
 		print_menu();
-		scanf("%d", &choice);
+
+		if (scanf("%d", &choice) != 1)
+		{
+			printf("Invalid choice\n");
+			clear_input();
+			continue;
+		}
+
+		if (choice == 0)
+			break;
+
 		handle_operation(choice);
-	} while (choice != 0);
+	}
 
 	printf("Bye!\n");
 
