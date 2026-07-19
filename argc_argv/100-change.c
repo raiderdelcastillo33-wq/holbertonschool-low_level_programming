a144982c0195ee46dd746f461437635863e8f996
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-07-19 20:15:27 +0200

    Print minimum number of coins

---
+++ argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the minimum number of coins for an amount
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success, 1 if the argument count is invalid
+ */
+int main(int argc, char *argv[])
+{
+	int cents;
+	int coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coins = cents / 25;
+	cents %= 25;
+
+	coins += cents / 10;
+	cents %= 10;
+
+	coins += cents / 5;
+	cents %= 5;
+
+	coins += cents / 2;
+	cents %= 2;
+
+	coins += cents;
+
+	printf("%d\n", coins);
+
+	return (0);
+}
