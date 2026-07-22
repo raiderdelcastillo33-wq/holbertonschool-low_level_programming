19bf2ac8edde8044a09407a3e3c239631533f48e
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-07-22 22:38:39 +0200

    Print a message before main executes

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - prints a message before main executes
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
