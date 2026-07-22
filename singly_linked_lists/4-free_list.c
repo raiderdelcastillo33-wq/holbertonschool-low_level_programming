99a396009ad76c99036ca143f4c80de633cfe611
Raider Del Castillo Abalos <raiderdelcastillo33@icloud.com>
2026-07-22 22:12:56 +0200

    Free a singly linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head->str);
+		free(head);
+		head = next;
+	}
+}
